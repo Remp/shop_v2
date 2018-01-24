deafa84c006c68686f96160877683a6c93dd1c92
Kiril <kirilrempov@gmail.com>
2018-01-24 21:39:57 +0200

    slider's fixes

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
-import Slider from './Slider';
+import Slider from './Slider.js';
 import ProductDescription from './ProductDescription.1.js';
 import 'font-awesome/css/font-awesome.min.css';
 import '../styles/ProductPage.css';
+import $ from 'jquery';
 
 class ProductPage extends Component{
     render(){
         return (
-            <div className="product-page">
-                <div className="close">
-                    <div className="close-btn" onClick={() => this.props.close_handler()}>
-                        <i className="fa fa-times"></i>
-                    </div>
-                </div>
+            <div ref={el => this.$page = $(el)} className="product-page">
                 <div className="product-content">
                     <Slider images={this.props.images} />
                     <ProductDescription 
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,42 +1,74 @@
 import React, {Component} from 'react';
 import 'font-awesome/css/font-awesome.css';
 import '../styles/Slider.css';
+import $ from 'jquery';
 
 class Slider extends Component{
     constructor(){
         super();
-        this.state = {
-            current: 0
-        }
+        this.current = 0;
+        this._resizeSlider = this._resizeSlider.bind(this);
+    }
+    componentDidMount(){
+        this._resizeSlider();
+        window.addEventListener('resize', this._resizeSlider);
+    }
+    componentWillUnmount(){
+        window.removeEventListener('resize', this._resizeSlider);
+    }
+    _resizeSlider(){
+        this.$controllers.css({
+            width: this.$slider.width() + 'px',
+            height: this.$slider.height() + 'px'
+        })
     }
     next(){
-        let current = this.state.current;
-        current = ++current % this.props.images.length;
-        this.setState({
-            current: current
+        this.current = this.current === this.props.images.length - 1 ? 0 : ++this.current;
+        this.$scroller.css({
+            transform: `translateX(${-this.current * this.$scroller.width()}px)`
         })
     }
     prev(){
-        let current = this.state.current;
-        current = current === 0 ? this.props.images.length - 1 : --current % this.props.images.length
-        this.setState({
-            current: current
+        this.current = this.current === 0 ? this.props.images.length - 1 : --this.current;
+        this.$scroller.css({
+            transform: `translateX(${-this.current * this.$scroller.width()}px)`
         })
     }
+    onmouseenter_handler(){
+        this.$controllers.fadeIn(10);
+    }
+    onmouseleave_handler(){
+        this.$controllers.fadeOut(10);
+    }
     render(){
-        const currentImage = `url(${this.props.images[this.state.current]})`;
-        const style = {
-            backgroundImage: currentImage
-        }
         return (
-            <div  className="slider" style={style}>
-                <div onClick={() => this.prev()}>
-                    <i className="fa fa-chevron-left"></i>
+            <div 
+                onMouseEnter={() => this.onmouseenter_handler()} 
+                onMouseLeave={() => this.onmouseleave_handler()} 
+                ref={el => this.$slider = $(el)} 
+                className="slider"
+            >
+                <div style={{display: 'none'}} ref={el => this.$controllers = $(el)} class="controllers">
+                    <button onClick={() => this.prev()} className='controller'>
+                        <i className='fa fa-chevron-left'></i>
+                    </button>
+                    <button onClick={() => this.next()} className='controller'>
+                        <i className='fa fa-chevron-right'></i>
+                    </button>
                 </div>
-                <div onClick={() => this.next()}>
-                    <i className="fa fa-chevron-right"></i>
-                </div>
+                <ul ref={el => this.$scroller = $(el)} className="scroller">
+                    {
+                        this.props.images.map(img => {
+                            return (
+                                <li onClick={() => this.onClick_handler()}  className="item-scroll">
+                                    <img src={img} alt=""/>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
+           
         )
     }
 }
